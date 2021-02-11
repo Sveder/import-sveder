60d8564af05c558248702d38eb034abc51dcfa2e
sveder <m@sveder.com>
2021-02-11 23:35:08 +0200

    Make this work again.

---
+++ sveder/__init__.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+def open_sveder_com():
+    webbrowser.open_new("http://sveder.com")
+
+if "__main__" == __name__:
+    open_sveder_com()
--- sveder/sveder.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-
-def open_sveder_com():
-    webbrowser.open_new("http://sveder.com")
-
-if "__main__" == __name__:
-    open_sveder_com()
